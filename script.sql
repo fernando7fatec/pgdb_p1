46f114552d39c997221be1dc6d009bdebce68f0e
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-10-29 09:59:00 -0300

    feat: Table created with the required attributes

--- script.sql
+++ script.sql
@@ -2,6 +2,21 @@
 -- 1 Base de dados e criação de tabela
 --escreva a sua solução aqui
 
+-- Create table students_performance
+-- with the required attributes
+CREATE TABLE students_performance(  
+    id SERIAL PRIMARY KEY,
+    age INT,
+    scholarship INT,
+    partner INT,
+    salary INT,
+    mother_edu INT,
+    father_edu INT,
+    read_freq INT,
+    read_freq_sci INT,
+    classroom INT,
+    grade INT);
+
 
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
